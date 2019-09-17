ad0c053a098cbb5b2cebe292c43cc1f84c85ae8d
FdelMazo <fdelmazo@fi.uba.ar>
2019-09-17 13:27:15 -0300

    TP2 comprime bien pruebas oneblk y largenum y alot y pad

--- TP2/main.cpp
+++ TP2/main.cpp
@@ -6,48 +6,65 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <climits>
 
 int main(int argc, char *argv[]) {
     std::ifstream input;
     input.open(argv[4], std::ios::in | std::ios::binary);
-    std::string line;
-    int blockn = atoi(argv[1]);
-
-    std::vector <uint32_t> block(blockn);
-    for (uint32_t i = 0; i < block.size(); i++) {
-        uint32_t myint;
-        input.read(reinterpret_cast<char *>(&myint), sizeof(uint32_t));
-        block[i] = be32toh(myint);
-    }
-
-    uint32_t min = *std::min_element(block.begin(), block.end());
-
-    for (uint32_t i = 0; i < block.size(); i++) {
-        block[i] -= min;
-    }
-
-    uint32_t max = *std::max_element(block.begin(), block.end());
-    int bits = ceil(log2(max));
-
-    min = htobe32(min);
-
     std::ofstream output;
     output.open(argv[5], std::ios::out | std::ios::binary);
-    output.write(reinterpret_cast<const char *>(&min), sizeof(uint32_t));
-    output.write(reinterpret_cast<const char *>(&bits), sizeof(uint8_t));
 
-    std::string pack;
-    for (uint32_t i = 0; i < block.size(); i++) {
-        std::string binary = std::bitset<sizeof(uint32_t)>(block[i]).to_string();
-        std::string importantBits = binary.substr(bits);
-        pack += importantBits;
+    int blockSize = atoi(argv[1]);
+
+    while( input.peek() != EOF ) {
+        std::vector <uint32_t> block(blockSize);
+        std::cerr << "Block to compress: ";
+        for (uint32_t i = 0; i < block.size(); i++) {
+            uint32_t myint;
+            input.read(reinterpret_cast<char *>(&myint), sizeof(uint32_t));
+            block[i] = be32toh(myint);
+            std::cerr << block[i] << " - ";
+        }
+        std::cerr << '\n';
+
+        uint32_t min = *std::min_element(block.begin(), block.end());
+        std::cerr << "Ref in block: " << min << "; ";
+
+        std::cerr << "Now turned to: ";
+        for (uint32_t i = 0; i < block.size(); i++) {
+            block[i] -= min;
+            std::cerr << block[i] << " - ";
+        }
+        std::cerr << '\n';
+
+        uint32_t max = *std::max_element(block.begin(), block.end());
+        int bitsToRepr = ceil(log2(max));
+
+        std::cerr << "Max in block: " << max << "; ";
+        std::cerr << "Bits neccesary to represent: " << bitsToRepr << "; ";
+
+        min = htobe32(min);
+        output.write(reinterpret_cast<const char *>(&min), sizeof(uint32_t));
+        output.write(reinterpret_cast<const char *>(&bitsToRepr), sizeof(uint8_t));
+
+        std::string pack;
+        for (uint32_t i = 0; i < block.size(); i++) {
+            std::string binary = std::bitset<sizeof(uint32_t) * CHAR_BIT>
+                    (block[i]).to_string();
+            int mask = binary.size() - bitsToRepr;
+            std::string importantBits = binary.substr(mask);
+            pack += importantBits;
+        }
+        while (pack.size() % CHAR_BIT != 0) {
+            pack += '0';
+        }
+        for (uint y = 0; y < pack.size(); y+=CHAR_BIT) {
+            std::string byte = pack.substr(y, CHAR_BIT);
+            unsigned long ul = strtoul(byte.c_str(), NULL, 2);
+            uint8_t x = (uint8_t) ul;
+            output.write(reinterpret_cast<const char *>(&x), sizeof(uint8_t));
+        }
     }
-
-    unsigned long ul;
-    ul = strtoul(pack.c_str(), NULL, 2);
-    uint8_t x = (uint8_t) ul;
-
-    output.write(reinterpret_cast<const char *>(&x), sizeof(uint8_t));
 
     input.close();
     output.close();
